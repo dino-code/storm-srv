81bab1295f6d41ec414cede6e4b4dcbd5db7fa09
Dino Becaj <dbecaj1@gmail.com>
2023-04-13 03:36:01 -0500

    digitalocean

--- storm_srv/settings.py
+++ storm_srv/settings.py
@@ -169,6 +169,27 @@ SIMPLE_JWT = {
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 STATIC_ROOT = 'staticfiles/'
 
 CORS_ALLOWED_ORIGINS = [
